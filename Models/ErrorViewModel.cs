128a944a58609e258c9aabe0b6012453f9c1c29c
Saranthorn0827 <64010827@kmitl.ac.th>
2023-04-18 20:03:53 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectKMITL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

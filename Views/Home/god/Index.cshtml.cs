d67df88976f0efe429e7f93199b47ae3a9e32ce9
Saranthorn0827 <64010827@kmitl.ac.th>
2023-04-18 20:31:34 +0700

    Update FindCafeteria Page

---
+++ Views/Home/god/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectKMITL.Views.Home.god
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

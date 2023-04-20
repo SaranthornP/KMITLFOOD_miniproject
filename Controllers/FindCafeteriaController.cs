08839693e86b9f0f51f2c1cffb6362635bd1a244
Saranthorn0827 <64010827@kmitl.ac.th>
2023-04-20 22:27:30 +0700

    Add function in Find Cafeteria Page

---
+++ Controllers/FindCafeteriaController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectKMITL.Models;
+
+namespace ProjectKMITL.Controllers
+{
+    public class FindCafeteriaController : Controller
+    {
+        // GET: FindCafeteria
+        public ActionResult Index()
+        {
+
+            Cafeteria c1 = new Cafeteria();
+            c1.Name = "โรงอาหารพระเทพ";
+            c1.Location = "3 ถ. ฉลองกรุง แขวงลำปลาทิว เขตลาดกระบัง กรุงเทพมหานคร 10520 (ใกล้ตึกพระเทพ, ตึก ECC)";
+            c1.Image = "fb.png";
+
+            var c2 = new Cafeteria();
+            c2.Name = "โรงอาหาร A";
+            c2.Location = "ECC";
+            c2.Image = "fb.png";
+
+            var c3 = new Cafeteria();
+            c3.Name = "โรงอาหารถื่นชงโค";
+            c3.Location = "ECC";
+            c3.Image = "fb.png";
+
+            var c4 = new Cafeteria();
+            c4.Name = "โรงอาหาร C";
+            c4.Location = "ECC";
+            c4.Image = "fb.png";
+
+
+            List<Cafeteria> allCafeteria = new List<Cafeteria>();
+            allCafeteria.Add(c1);
+            allCafeteria.Add(c2);
+            allCafeteria.Add(c3);
+            allCafeteria.Add(c4);
+
+            return View(allCafeteria);
+        }
+
+        // GET: FindCafeteria/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: FindCafeteria/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FindCafeteria/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FindCafeteria/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FindCafeteria/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FindCafeteria/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FindCafeteria/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Cafeteria.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectKMITL.Models
+{
+    public class Cafeteria
+    {
+
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public string Image { get; set; } 
+    }
+}

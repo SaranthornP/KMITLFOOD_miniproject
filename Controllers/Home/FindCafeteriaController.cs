651c0b21ecec804b565d28c4ad0ce5a9685937b5
Saranthorn0827 <64010827@kmitl.ac.th>
2023-04-25 21:18:44 +0700

    Update login function

--- Controllers/Home/FindCafeteriaController.cs
+++ Controllers/Home/FindCafeteriaController.cs
@@ -2,11 +2,13 @@
 using Microsoft.AspNetCore.Mvc;
 using ProjectKMITL.Models;
 
-namespace ProjectKMITL.Controllers
+namespace ProjectKMITL.Controllers.Home
 {
+    [Route("Home/[controller]/[action]")]
     public class FindCafeteriaController : Controller
     {
         // GET: FindCafeteria
+        [Route("/Home/FindCafeteria")]
         public ActionResult Index()
         {
             if (HttpContext.Session.GetString("UserName") == null)
@@ -16,22 +18,22 @@ namespace ProjectKMITL.Controllers
             Cafeteria c1 = new Cafeteria();
             c1.Name = "โรงอาหารพระเทพ";
             c1.Location = "3 ถ. ฉลองกรุง แขวงลำปลาทิว เขตลาดกระบัง กรุงเทพมหานคร 10520 (ใกล้ตึกพระเทพ, ตึก ECC)";
-            c1.Image = "c1.png";
+            c1.Image = "LocationMap/c1.png";
 
             var c2 = new Cafeteria();
             c2.Name = "โรงอาหาร A";
             c2.Location = "ECC";
-            c2.Image = "c2.png";
+            c2.Image = "LocationMap/c2.png";
 
             var c3 = new Cafeteria();
             c3.Name = "โรงอาหารถิ่นชงโค";
             c3.Location = "ECC";
-            c3.Image = "c3.png";
+            c3.Image = "LocationMap/c3.png";
 
             var c4 = new Cafeteria();
             c4.Name = "โรงอาหาร C";
             c4.Location = "ECC";
-            c4.Image = "c4.png";
+            c4.Image = "LocationMap/c4.png";
 
 
             List<Cafeteria> allCafeteria = new List<Cafeteria>();

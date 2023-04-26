0049b12fe6d422821dbd493e9ac6c46f7c028166
Saranthorn0827 <64010827@kmitl.ac.th>
2023-04-27 00:19:43 +0700

    add Cafeteria Image

--- Controllers/Home/FindCafeteriaController.cs
+++ Controllers/Home/FindCafeteriaController.cs
@@ -18,25 +18,25 @@ namespace ProjectKMITL.Controllers.Home
             Cafeteria c1 = new Cafeteria();
             c1.CafeteriaImg = "Restaurant/b1.png";
             c1.Name = "โรงอาหารพระเทพ";
-            c1.Location = "3 ถ. ฉลองกรุง แขวงลำปลาทิว เขตลาดกระบัง กรุงเทพมหานคร 10520 (ใกล้ตึกพระเทพ, ตึก ECC)";
+            c1.Location = "3 ถ. ฉลองกรุง แขวงลำปลาทิว เขตลาดกระบัง กรุงเทพมหานคร 10520 (ข้างตึกพระเทพ, ใกล้ตึก ECC)";
             c1.Image = "LocationMap/c1.png";
 
             var c2 = new Cafeteria();
             c2.CafeteriaImg = "Restaurant/b2.png";
             c2.Name = "โรงอาหาร A";
-            c2.Location = "ECC";
+            c2.Location = "3 ถ. ฉลองกรุง แขวงลาดกระบัง เขตลาดกระบัง กรุงเทพมหานคร 10520 (ข้างตึก A, ใกล้ตึก HM)";
             c2.Image = "LocationMap/c2.png";
 
             var c3 = new Cafeteria();
             c3.CafeteriaImg = "Restaurant/b3.png";
             c3.Name = "โรงอาหารถิ่นชงโค";
-            c3.Location = "ECC";
+            c3.Location = "3 ถ. ฉลองกรุง แขวงลาดกระบัง เขตลาดกระบัง กรุงเทพมหานคร 10520 (ใกล้ตึกวิศวกรรมการวัดคุมม)";
             c3.Image = "LocationMap/c3.png";
 
             var c4 = new Cafeteria();
             c4.CafeteriaImg = "Restaurant/b4.png";
             c4.Name = "โรงอาหาร C";
-            c4.Location = "ECC";
+            c4.Location = "3 ถนน ฉลองกรุง แขวงลาดกระบัง เขตลาดกระบัง กรุงเทพมหานคร 10520 (ตรงข้ามตึกโหล)";
             c4.Image = "LocationMap/c4.png";
 
 

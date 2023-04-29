84eaa32c9b4a0c0b7e91a951fbd90b1a826479b4
Saranthorn0827 <64010827@kmitl.ac.th>
2023-04-29 20:38:36 +0700

    Add work

---
+++ Controllers/WorkController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectKMITL.Data;
+using ProjectKMITL.Models;
+
+namespace ProjectKMITL.Controllers
+{
+    public class WorkController : Controller
+    {
+
+        private readonly OrderDbContext _context;
+
+        public WorkController(OrderDbContext context)
+        {
+            _context = context;
+
+        }
+            public IActionResult Index()
+        {
+            IEnumerable<OrderModel> allOrder = _context.Orders;
+            var list = allOrder.Where(x => x.NameDepository == "");
+
+            return View(list);
+        }
+    }
+}
